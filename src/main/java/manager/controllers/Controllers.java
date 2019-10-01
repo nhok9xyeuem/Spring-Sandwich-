b910a7474089aa36b9cc6484806e642afcdbd621
nhok9xyeuem <nhuconghaui@gmail.com>
2019-10-01 21:50:03 +0700

    sandwich

--- src/main/java/manager/controllers/Controllers.java
+++ src/main/java/manager/controllers/Controllers.java
@@ -1,4 +1,23 @@
 package manager.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
 public class Controllers {
+
+    @RequestMapping("/")
+     public String index(){
+        return "index";
+    }
+    @PostMapping ("/sandwich")
+    public  String save (@RequestParam ("condiment") String[] condiment , Model model  ){
+       model.addAttribute("condiment", condiment);
+        return "sandwich";
+    }
+
+
 }
